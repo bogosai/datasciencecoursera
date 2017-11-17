dcbe6b7d1ac5812a814068e8b965ba80f338f2d8
Alexey Bogoslovsky <abogoslo@mail.ru>
2017-11-17 04:24:20 +0300

    w1

--- m2_rp/w1.R
+++ m2_rp/w1.R
@@ -1,11 +1,18 @@
 pollutantmean <- function(directory,pollutant,id = 1:332) {
         flist <-list.files(directory)
         data <-data.frame()
-        for (i in id) {
-                data <- rbind(data,read.csv(file.path(directory,flist[i]),skip=1,header=FALSE,nrow=5))
-        }
-        colnames(data) <- c("Date","sulfate","nitrate","ID")
-        print(data[4])
         
-
+        # for (i in 1:length(flist)) {
+        for (i in 1:3) {        
+                data <- rbind(data,read.csv(file.path(directory,flist[i]),skip=1,header=FALSE))
         }
+        colnames(data) <- c("Date","sulfate","nitrate","ids")
+
+        z<-id
+
+        
+        # q<-data[!is.na(data[pollutant]) & data["ids"] %in% z]
+        data[data["ids"]>1]
+
+}
+pollutantmean("specdata","sulfate",1:3)
