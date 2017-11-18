6933f74b23f89e13601563b0993d2e3793027902
Alexey Bogoslovsky <abogoslo@mail.ru>
2017-11-18 14:08:47 +0300

    w1

--- m2_rp/w1_2.R
+++ m2_rp/w1_2.R
@@ -9,7 +9,7 @@ complete <- function(directory,ids=1:332) {
         }
         
         colnames(data) <- c("Date","sulfate","nitrate","id")
-        sbset <- data[data[,"id"] %in% rng & !is.na(data[,2]) & !is.na(data[,2]),]
+        sbset <- data[data[,"id"] %in% rng & !is.na(data[,2]) & !is.na(data[,3]),]
         fctr <- factor(sbset[,"id"],levels=rng)
         dframe <- data.frame(table(fctr))
         colnames(dframe) <- c("id","nobs")
