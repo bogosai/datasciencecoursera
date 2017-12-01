5e33af483eec130377e4c022e6a3d787369d6169
Alexey Bogoslovsky <abogoslo@mail.ru>
2017-12-01 04:20:27 +0300

    assignment3

--- m2_rp/assignment3/rankhospital2.R
+++ m2_rp/assignment3/rankhospital2.R
@@ -1,52 +1,51 @@
 rankhospital <- function(state,outcome,num=61) {
         
+        outcome <- tolower(outcome)
+        
+        if (outcome == "heart attack") {
+                i <- 11
+        } else if (outcome == "heart failure") {
+                i <- 17
+        } else if (outcome == "pneumonia") {
+                i <- 23
+        }
+        
+        
         readData <- data.frame()
-        data <- data.frame()
-        fdata <- data.frame()
-        readData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-        data <- readData[,c(2,7,15,21,27)]
-        colnames(data) <- c("Hospital","State","Heart Attack","Heart Failure","Pneumonia")
-        nm <- names(data)
+        readData <- read.csv("outcome-of-care-measures.csv", colClasses = "character",stringsAsFactors=FALSE)
+        
         
         # if (sum(nm==outcome) ==0) stop("Invalid outcome")
         # if (nrow(data[data[,"State"]==state,])==0) stop("Invalid state")
         
-        data <- data[data[,"State"]==state & !is.na(data[,outcome]) & data[,outcome]!="Not Available",]
-        fdata <-data[,"Hospital"]
-        outcome <- as.numeric(data[[outcome]])
-        fdata <- cbind(fdata,as.numeric(outcome))
-        fdata
-        #data <- data[order(data[,outcome],data[,"Hospital"]),]
         
-        # ldata <- as.numeric(levels(factor(data[,outcome])))
-        # odata <- order(ldata)
-        # 
-        # fdata <- cbind(ldata[odata],seq_along(1:length(odata)))
-        # fdata <- data.frame(fdata)
-        # 
-        # colnames(fdata) <- c("value","order")
-        # fdata
-        # if (num=="best") {
-        #         f<-head(fdata,1)
-        #         f<-f$value
-        # 
-        # } else if (num=="worst") {
-        #         f<-tail(fdata,1)
-        #         f<-f$value
-        # } else {
-        #         f <- fdata[fdata[,"order"]==num,]
-        #         f <- f$value
-        # }
-        # 
-        # data <- data[data[,outcome]==f,]
-        # data <- data[order(data[,"Hospital"]),]
-        # data
+        readData <- readData[readData[,"State"]==state & !is.na(readData[,i]) & readData[,i]!="Not Available",]
         
         
-        
-        
-        # data <- head(data[order(data[,"Hospital"]),],1)
-        # 
-        # Hospital.Name <<- data$Hospital
-        
+        lens<- length(readData[,i])
+        readData <- readData[order(readData[,i],readData[,"Hospital.Name"]),]
+        readData <- cbind(seq_along(1:lens),readData)
+
+
+        if(lens >= num | is.character(num)) {
+                if (num=="best") {
+
+                        f <- head(readData,1)
+                        f <- f$'Hospital.Name'
+
+                } else if (num=="worst") {
+
+                        f <- tail(readData,1)
+                        f <- f$'Hospital.Name'
+
+                } else {
+
+                        f <- readData[readData[,1]==num,]
+                        f <- f$'Hospital.Name'
+                }
+        } else {f<-"NA"}
+
+
+        Hospital.Name <<- f
+        Hospital.Name
 }
